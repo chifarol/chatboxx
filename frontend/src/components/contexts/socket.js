e455ecb71124b25d16be25237e79b2a8c5a5a9e2
chifarol <ilodigwechinaza@gmail.com>
2022-10-11 02:48:52 -0700

    fixing auth and socket issue

--- frontend/src/components/contexts/socket.js
+++ frontend/src/components/contexts/socket.js
@@ -13,43 +13,42 @@ export const SocketContextProvider = ({ children }) => {
   let userLocal = JSON.parse(sessionStorage.getItem("user"));
   if (!userLocal) {
     return;
-  } else {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        auth: userLocal.token,
-      },
-    };
-    socket.on("connect", () => {
-      axios
-        .get(`/api/user?username=${userLocal.username}`, config)
-        .then((res) => {
-          // join all room ids in main users room activity array i.e userObject.rooms
-          socket.emit("join_rooms", { rooms: res.data.user.rooms });
-        })
-        .catch((e) => {
-          console.log(e);
-          return new Error("couldn't fetch user & rooms =>", e);
-        });
-      console.log("socketId", socket.id);
-      socketId = socket.id;
-      // add username to list tracking online users on disconnect
-      socket.emit("online", { username: userLocal.username });
-    });
-
-    socket.on("error", function (err) {
-      if (err.description) console.log(err.description);
-      else console.log(err);
-    });
-
-    socket.on("disconnect", (reason) => {
-      console.log("socket connection disconnecting...", reason);
-      // remove username from list tracking online users on disconnect
-      socket.emit("offline", { username: userLocal.username });
-    });
-    const value = { socket, socketId };
-    return (
-      <SocketContext.Provider value={value}>{children}</SocketContext.Provider>
-    );
   }
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      auth: userLocal.token,
+    },
+  };
+  socket.on("connect", () => {
+    axios
+      .get(`/api/user?username=${userLocal.username}`, config)
+      .then((res) => {
+        // join all room ids in main users room activity array i.e userObject.rooms
+        socket.emit("join_rooms", { rooms: res.data.user.rooms });
+      })
+      .catch((e) => {
+        console.log(e);
+        return new Error("couldn't fetch user & rooms =>", e);
+      });
+    console.log("socketId", socket.id);
+    socketId = socket.id;
+    // add username to list tracking online users on disconnect
+    socket.emit("online", { username: userLocal.username });
+  });
+
+  socket.on("error", function (err) {
+    if (err.description) console.log(err.description);
+    else console.log(err);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("socket connection disconnecting...", reason);
+    // remove username from list tracking online users on disconnect
+    socket.emit("offline", { username: userLocal.username });
+  });
+  const value = { socket, socketId };
+  return (
+    <SocketContext.Provider value={value}>{children}</SocketContext.Provider>
+  );
 };
